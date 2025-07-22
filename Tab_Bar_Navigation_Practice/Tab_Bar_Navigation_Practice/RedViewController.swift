d73340e18787ea411fd11e0b56dfc047c2a47cd6
anmol0031-debug <anmol0031.be23@chitkara.edu.in>
2025-07-22 09:26:36 +0530

    21/7/2025 Work

---
+++ Tab_Bar_Navigation_Practice/Tab_Bar_Navigation_Practice/RedViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tab_Bar_Navigation_Practice
+//
+//  Created by Anmol Mehta on 21/07/25.
+//
+
+import UIKit
+
+class RedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Tab_Bar_Navigation_Practice/Tab_Bar_Navigation_PracticeTests/Tab_Bar_Navigation_PracticeTests.swift
@@ -0,0 +1,17 @@
+//
+//  Tab_Bar_Navigation_PracticeTests.swift
+//  Tab_Bar_Navigation_PracticeTests
+//
+//  Created by Anmol Mehta on 21/07/25.
+//
+
+import Testing
+@testable import Tab_Bar_Navigation_Practice
+
+struct Tab_Bar_Navigation_PracticeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
